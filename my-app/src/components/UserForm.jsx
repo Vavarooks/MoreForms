b44c85cbeb6c6e0f10b51f0b2f112e881446df61
Vanessa Emerick <vavarooks123@gmail.com>
2022-10-31 18:04:10 -0700

    Final edits to formS

--- my-app/src/components/UserForm.jsx
+++ my-app/src/components/UserForm.jsx
@@ -7,14 +7,22 @@ const UserForm = (props) => {
     const [lastName, setLastname] = useState("");
     const [lastError, setLastError] = useState("");
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
     const [password, setPassword] = useState("");
-    const [conPassword, confirmPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState("");
+    const [confirmPasswordError, setconfirmPasswordError] = useState("");
     const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
 
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = { firstName, lastName, email, password, conPassword };
+        const newUser = { firstName, lastName, email, password, confirmPassword };
 
+        
+        console.log("Welcome", newUser);
+        setHasBeenSubmitted(true);
+    };
+    const firstUser = (e) =>{
         setFirstname(e.target.value);
         if (e.target.value.length < 1) {
             setFirstError("First name is required!");
@@ -23,7 +31,9 @@ const UserForm = (props) => {
         } else {
             setFirstError("");
         }
+    }
 
+    const lastUser = (e) => {
         setLastname(e.target.value);
         if (e.target.value.length < 1) {
             setLastError("Last name is required!");
@@ -32,10 +42,38 @@ const UserForm = (props) => {
         } else {
             setLastError("");
         }
+    }
 
-        console.log("Welcome", newUser);
-        setHasBeenSubmitted(true);
-    };
+    const emailUser = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length < 1){
+            setEmailError("Email is required!");
+        }else if (e.target.value.length < 5){
+            setEmailError("Email must be longer than five letters!");
+        }else{
+            setEmailError("");
+        }
+    }
+
+    const passUser = (e) => {
+        setPassword(e.target.value);
+        if(e.target.value.length < 1){
+            setPasswordError("Password is required!");
+        }else if (e.target.value.length < 8){
+            setPasswordError("Password must be longer than eight letters!");
+        }else{
+            setPasswordError("");
+        }
+    }
+
+    const confirmUserPass = (e) => {
+        setconfirmPassword(e.target.value);
+        if(e.target.value !== password){
+            setconfirmPasswordError("Passwords must match!!!");
+        }else{
+            setconfirmPasswordError("");
+        }
+    }
 
     const formMessage = () => {
         if (hasBeenSubmitted) {
@@ -52,7 +90,7 @@ const UserForm = (props) => {
                     <h3>{formMessage()}</h3>
                     <div class="mb-3">
                         <label class="form-label">First Name: </label>
-                        <input class="form-control" type="text" onChange={(e) => setFirstname(e.target.value, "firstname")} />
+                        <input class="form-control" type="text" onChange={firstUser} />
                         {
                             firstError ?
                                 <p style={{ color: 'red' }}>{firstError}</p> : ''
@@ -60,7 +98,7 @@ const UserForm = (props) => {
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Last Name: </label>
-                        <input class="form-control" type="text" onChange={(e) => setLastname(e.target.value, "lastname")} />
+                        <input class="form-control" type="text" onChange={lastUser} />
                         {
                             lastError ?
                                 <p style={{ color: 'red' }}>{lastError}</p> : ''
@@ -68,15 +106,27 @@ const UserForm = (props) => {
                     </div>
                     <div>
                         <label class="form-label">Email Address: </label>
-                        <input class="form-control" type="email" onChange={(e) => setEmail(e.target.value, "email")} />
+                        <input class="form-control" type="email" onChange={emailUser} />
+                        {
+                            emailError ?
+                                <p style={{ color: 'red' }}>{emailError}</p> : ''
+                        }
                     </div>
                     <div>
                         <label class="form-label">Password: </label>
-                        <input class="form-control" type="password" onChange={(e) => setPassword(e.target.value)} />
+                        <input class="form-control" type="password" onChange={passUser} />
+                        {
+                            passwordError ?
+                                <p style={{ color: 'red' }}>{passwordError}</p> : ''
+                        }
                     </div>
                     <div>
                         <label class="form-label">Confirm Password: </label>
-                        <input class="form-control" type="password" onChange={(e) => confirmPassword(e.target.value)} />
+                        <input class="form-control" type="password" onChange={confirmUserPass} />
+                        {
+                            confirmPasswordError ?
+                                <p style={{ color: 'red' }}>{confirmPasswordError}</p> : ''
+                        }
                     </div>
                     <input class="form-control" type="submit" value="Create User" />
                 </form>
@@ -87,6 +137,7 @@ const UserForm = (props) => {
                 <p>Last Name: {lastName}</p>
                 <p>Email: {email}</p>
                 <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
             </div>
         </>
     );
